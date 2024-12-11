87a51ce2b44321026287ab9b084cee76007d0a39
soniagkalliath <soniageorge111@gmail.com>
2024-12-11 12:17:16 +0530

    delete & profile p1

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,10 +1,64 @@
 //rafce
-import React,{useState} from 'react'
+import React,{useEffect, useState} from 'react'
 import { Collapse } from 'react-bootstrap'
 import uploadImg from '../assets/uploadImg.jpg'
+import SERVER_BASE_URL from '../services/serverUrl';
 
 const Profile = () => {
   const [open, setOpen] = useState(false);
+  const [preview,setPreview] = useState("")
+  const [existingProfilePic,setExisitingProfilePic] = useState("")
+  // profilePic key of userDeatils is used to store uploaded user profile pic file
+  const [userDetails,setUserDetails] = useState({
+    username:"",email:"",password:"",github:"",linkedin:"",profilePic:""
+  })
+  console.log(userDetails);
+  //get exisiting user details from session and store it to userDetails state
+  useEffect(()=>{
+    if(sessionStorage.getItem("user")){
+      const user = JSON.parse(sessionStorage.getItem("user"))
+      setUserDetails({
+        ...userDetails,username:user.username,email:user.email,password:user.password,github:user.github,linkedin:user.linkedin
+      })
+      setExisitingProfilePic(user.profilePic)
+    }
+  },[open])
+
+  //generate url for upload profile pic
+  useEffect(()=>{
+    if(userDetails.profilePic){
+      setPreview(URL.createObjectURL(userDetails.profilePic))
+    }else{
+      setPreview("")
+    }
+  },[userDetails.profilePic])
+
+  const handleUserUpdate = async ()=>{
+    // 1. get all user details
+    const {username,email,password,github,linkedin,profilePic} = userDetails
+    // if text filed have value
+    if(github && linkedin){
+      // req body
+      const reqBody = new FormData()
+      reqBody.append("username",username)
+      reqBody.append("email",email)
+      reqBody.append("password",password)
+      reqBody.append("github",github)
+      reqBody.append("linkedin",linkedin)
+      preview ? reqBody.append("profilePic",profilePic) :reqBody.append("profilePic",existingProfilePic)
+      //reqheader
+      const token = sessionStorage.getItem("token")
+      if(token){
+        const reqHeader = {
+          "Content-Type":"multipart/form-data",
+          "Authorization": `Bearer ${token}`
+        }
+        // make api call
+      }
+    }else{
+      alert("Please fill the form completetly!!!!")
+    }
+  }
 
   return (
     <>
@@ -16,17 +70,22 @@ const Profile = () => {
         <div className='row container-fluid align-items-center justify-content-center shadow p-2 rounded' id="example-collapse-text">
           {/* upload pic */}
           <label className='text-center'>
-            <input style={{display:'none'}} type="file" />
-            <img width={'200px'} height={'200px'} className='rounded-circle' src={uploadImg} alt="" />
+            <input onChange={e=>setUserDetails({...userDetails,profilePic:e.target.files[0]})} style={{display:'none'}} type="file" />
+            {
+              existingProfilePic=="" ?
+              <img width={'200px'} height={'200px'} className='rounded-circle' src={preview?preview:uploadImg} alt="" />
+              :
+              <img width={'200px'} height={'200px'} className='rounded-circle' src={preview?preview:`${SERVER_BASE_URL}/uploads/${existingProfilePic}`} alt="" />
+            }
           </label>
           <div className="mb-2 w-100">
-            <input type="text" placeholder='User GITHUB Link' className="form-control" />
+            <input value={userDetails.github} onChange={e=>setUserDetails({...userDetails,github:e.target.value})} type="text" placeholder='User GITHUB Link' className="form-control" />
           </div>
           <div className="mb-2 w-100">
-            <input type="text" placeholder='User LINKEDIN Link' className="form-control" />
+            <input value={userDetails.linkedin} onChange={e=>setUserDetails({...userDetails,linkedin:e.target.value})} type="text" placeholder='User LINKEDIN Link' className="form-control" />
           </div>
           <div className="d-grid w-100">
-            <button className="btn btn-warning">Update</button>
+            <button onClick={handleUserUpdate} className="btn btn-warning">Update</button>
           </div>
         </div>
       </Collapse>
--- src/components/View.jsx
+++ src/components/View.jsx
@@ -2,7 +2,7 @@
 import React, { useContext, useEffect, useState } from 'react'
 import Add from './Add'
 import Edit from './Edit'
-import { userProjectsAPI } from '../services/allAPI'
+import { deleteProjectAPI, userProjectsAPI } from '../services/allAPI'
 import { addProjectContext, editProjectContext } from '../contexts/ContextShare'
 
 const View = () => {
@@ -35,6 +35,24 @@ const View = () => {
     }
   }
   //4. display the array in jsx
+
+  const removeProject = async (id)=>{
+    const token = sessionStorage.getItem("token")
+    if(token){
+        const reqHeader = {
+          "Authorization": `Bearer ${token}`
+        }
+      try{
+        const result = await deleteProjectAPI(id,reqHeader)
+        if(result.status==200){
+          getUserProject()
+        }
+      }catch(err){
+        console.log(err);      
+      }
+    }
+  }
+
   return (
     <>
     <div className="d-flex justify-content-between mt-3">
@@ -50,7 +68,7 @@ const View = () => {
               <div className="d-flex align-items-center">
                   <div> <Edit project={project} /> </div>
                   <button className="btn"> <a href={project?.github} target='_blank'> <i className="fa-brands fa-github"></i> </a> </button>
-                  <button className="btn"><i className="fa-solid fa-trash text-danger"></i></button>
+                  <button onClick={()=>removeProject(project?._id)} className="btn"><i className="fa-solid fa-trash text-danger"></i></button>
                 </div>
             </div>
           ))
